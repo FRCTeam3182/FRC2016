b70b7b567549c39357602866ccdff70b09968ac2
Team 3182 <team3182@gmail.com>
2016-01-24 16:50:23 -0500

    wtf is dirty_worktree anyway

---
+++ src/org/usfirst/frc/team3182/robot/commands/AutoCollect.java
@@ -0,0 +1,60 @@
+package org.usfirst.frc.team3182.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3182.robot.Robot;
+import org.usfirst.frc.team3182.robot.RobotMap;
+import org.usfirst.frc.team3182.robot.subsystems.Collector;
+import org.usfirst.frc.team3182.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+/**
+ *
+ */
+public class AutoCollect extends Command {
+
+	Ultrasonic u1 = new Ultrasonic(RobotMap.ultraSonic1_ping, RobotMap.ultraSonic1_receive);
+	Ultrasonic u2 = new Ultrasonic(RobotMap.ultraSonic2_ping, RobotMap.ultraSonic2_receive);
+	
+//	Collector collector = Robot.collector;
+//	Drivetrain drivetrain = Robot.drivetrain;
+	
+	long startTime;
+	
+    public AutoCollect() {
+        // Use requires() here to declare subsystem dependencies
+//        requires(Robot.drivetrain);
+//        requires(Robot.collector);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	startTime = System.currentTimeMillis();
+//    	collector.collect();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	long finalTime = System.currentTimeMillis();
+    	
+//        return collector.isSwitchSet() || (finalTime - startTime > 2000);
+    	return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+//    	collector.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ src/org/usfirst/frc/team3182/robot/commands/TestAutoCommand.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team3182.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3182.robot.Robot;
+
+/**
+ *
+ */
+public class TestAutoCommand extends Command {
+	
+	long startTime;
+    public TestAutoCommand() {
+         //Use requires() here to declare subsystem dependencies
+        requires(Robot.drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	 startTime = System.currentTimeMillis();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drivetrain.drive(0.5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	long finalTime = System.currentTimeMillis();
+        return finalTime-startTime >= 5000;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
+
+
