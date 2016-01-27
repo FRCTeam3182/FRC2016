c8d91fe33b408128871b1ee6c8865504221e34da
Team3182 <Team3182@192.168.1.141>
2016-01-26 19:47:00 -0500

    DefAuto

---
+++ src/org/usfirst/frc/team3182/robot/commands/DefaultAuto.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team3182.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DefaultAuto extends Command {
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
