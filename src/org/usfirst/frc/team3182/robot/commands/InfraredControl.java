a13b862948d6437afbdb555cd2b168c974c33725
Team 3182 <team3182@gmail.com>
2016-02-15 18:37:35 -0500

    Update infrared distance calculations using best fit equation

--- src/org/usfirst/frc/team3182/robot/commands/InfraredControl.java
+++ src/org/usfirst/frc/team3182/robot/commands/InfraredControl.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team3182.robot.commands;
 
-import java.util.ArrayList;
-
 import org.usfirst.frc.team3182.robot.Robot;
-import org.usfirst.frc.team3182.robot.utils.Point;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -20,34 +17,15 @@ public class InfraredControl extends Command {
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		ArrayList<Point> points = Robot.infrared.getPoints();
 		double i1Voltage = Robot.infrared.getIr1().getVoltage();
 		double i2Voltage = Robot.infrared.getIr2().getVoltage();
-		for (Point p : points){
-			if (i1Voltage < p.getY()){
-				Point p1 = points.get(points.indexOf(p) - 1);
-				Point p2 = p;
-				double slope = calcSlope(p1, p2);
-				estDis1 = p1.getX() + ((i1Voltage - p1.getY()) / slope);
-			}
-			if (i2Voltage < p.getY()){
-				Point p1 = points.get(points.indexOf(p) - 1);
-				Point p2 = p;
-				double slope = calcSlope(p1, p2);
-				estDis2 = p1.getX() + ((i1Voltage - p1.getY()) / slope);
-			}
-			
-			
-		}
+		
+		estDis1 = 57.653*(Math.pow(i1Voltage, -0.9891));
+		estDis2 = 57.653*(Math.pow(i2Voltage, -0.9891));
+
 
 	}
-	
-	private double calcSlope(Point p1, Point p2){
-		double deltaY = p2.getY() - p1.getY();
-		double deltaX = p2.getX() - p1.getX();
-		
-		return (deltaY / deltaX);
-	}
+
 
 	@Override
 	protected boolean isFinished() {
--- src/org/usfirst/frc/team3182/robot/subsystems/Infrared.java
+++ src/org/usfirst/frc/team3182/robot/subsystems/Infrared.java
@@ -1,10 +1,7 @@
 package org.usfirst.frc.team3182.robot.subsystems;
 
-import java.util.ArrayList;
 
-import org.usfirst.frc.team3182.robot.commands.ArmControl;
 import org.usfirst.frc.team3182.robot.commands.InfraredControl;
-import org.usfirst.frc.team3182.robot.utils.Point;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,24 +11,11 @@ public class Infrared extends Subsystem {
 	public AnalogInput ir1 = new AnalogInput(5); // Check channel input
 	public AnalogInput ir2 = new AnalogInput(6);
 	
-	public static ArrayList<Point> points = new ArrayList<Point>(); // x is distance in cm, y is voltage
 	
 	public Infrared() {
-		populatePointArray();
-	}
-	
-	private void populatePointArray(){
-		points.add(new Point(15, 2.75));
-		points.add(new Point(20, 2.5));
-		points.add(new Point(30, 2.0));
-		points.add(new Point(40, 1.5));
-		points.add(new Point(50, 1.25));
-		points.add(new Point(60, 1.15));
-		points.add(new Point(70, 0.9));
-		points.add(new Point(80, 0.8));
-		points.add(new Point(90, 0.75));
 		
 	}
+	
 	
 	@Override
 	protected void initDefaultCommand() {
@@ -48,8 +32,5 @@ public class Infrared extends Subsystem {
 		return ir2;
 	}
 	
-	public static ArrayList<Point> getPoints() {
-		return points;
-	}
 
 }
