b7aa1ac48259ab1ae156ffcd69448be9d532ec22
Team 3182 <team3182@gmail.com>
2016-03-16 20:32:26 -0400

    updated with testing instructions

--- src/org/usfirst/frc/team3182/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team3182/robot/subsystems/Arm.java
@@ -17,6 +17,20 @@ import edu.wpi.first.wpilibj.PIDController;
  */
 
 public class Arm extends Subsystem {
+	/*
+	 * alright so the PID doesn't work
+	 * the problem is in the pot or in the PID
+	 * test by printing out armControl.get() to see if it's actually outputting anything
+	 * if this doesn't work mess with the PID to make sure everything is enabled (it should be)
+	 * if this doesn't work copy and paste everything relevant from drivetrain
+	 * if this doesn't work write your own feedback loop (it's not hard to write out a shitty PI controller,
+	 * 		you don't need a D for this application
+	 * if that doesn't work tell me and i'll write my own feedback loop
+	 * if that doesn't work we'll go back to normal joystick control but with the powerglove
+	 * 
+	 * also maybe test the motor
+	 * and possibly check armControl.getSetpoint() but i don't think that's the issue
+	 */
 	
 	Talon armMotor = new Talon(RobotMap.armMotor);
 	AnalogInput pmeter = new AnalogInput(RobotMap.armPotentiometer);
@@ -50,8 +64,10 @@ public class Arm extends Subsystem {
 	public void runRaw(double speed) {
 		armMotor.set(speed);
 	}
-	
+	//don't be dumb and set this too low or high
 	public void set(double theta) {
+		System.out.println("Setting: "+theta + " "+armControl.get());
+		armControl.enable();
 		armControl.setSetpoint(theta);
 		
 	}
@@ -63,6 +79,7 @@ public class Arm extends Subsystem {
 	
 	public void stop() {
 		armMotor.set(0);
+		armControl.disable();
 	}
 
 }
